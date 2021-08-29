985d027965f3a64b4bee4d16939853eef3f18d3b
Manu A T <atmanu7@gmail.com>
2021-08-29 22:13:58 +0530

    'test3'

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-app.use(express.static('dist'));
-router.get('/',function(req,res){
-  res.sendFile('dist/index.html');
-  //__dirname : It will resolve to your project folder.
-});
+// set up ======================================================================
+var express = require('express');
+var app = express(); 						// create our app w/ express
+var mongoose = require('mongoose'); 				// mongoose for mongodb
+var port = process.env.PORT || 8080; 				// set the port
+var database = require('./config/database'); 			// load the database config
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cors = require('cors');
+// configuration ===============================================================
 
-// router.get('/about',function(req,res){
-//   res.sendFile(path.join(__dirname+'/about.html'));
-// });
+// mongoose.connect('mongodb+srv://manu:cat007@@cluster0-axhr4.mongodb.net/', {dbName: 'manu'});
 
-// router.get('/sitemap',function(req,res){
-//   res.sendFile(path.join(__dirname+'/sitemap.html'));
-// });
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 8080);
 
-console.log('Running at Port 8080');
+mongoose.connect(database.remoteUrl,(err)=>{
+    console.log("err",err)
+}); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+
+app.use(cors());
+app.use(express.static('dist/lms'));
+
+app.get('/',(req,res)=>{
+res.sendFile('dist/lms/index.html');
+})
+
+// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
