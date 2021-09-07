9a904a6fb31ddf689d622157ce725e1bb1c10f26
Manu A T <atmanu7@gmail.com>
2021-09-07 09:57:34 +0530

    delete old files

--- README.md
+++
@@ -1,7 +0,0 @@
-# basic_node_server
-basic server setup
-
-how to run:
-1.clone it
-2.npm install
-3.nodemon (development)
--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    remoteUrl : 'mongodb+srv://manu:cat007@cluster0-axhr4.mongodb.net/manu',
-    localUrl: 'mongodb://localhost/manu'
-};
